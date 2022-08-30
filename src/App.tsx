7fc1d84a34878370df9247c54b55a8d775073612
msseleznev <mikestm8979@gmail.com>
2022-08-30 17:56:59 +0400

    added form styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,152 @@
 import React from 'react';
-import './App.scss';
+import s from './App.module.scss';
+import ReCAPTCHA from "react-google-recaptcha";
 
 const App = () => {
     return (
-        <form className="box">
-            <h5 className="title is-5">
-                Регистрация
-            </h5>
-            <section className="block m-6">
-                <p className="mb-1"><strong>Вы регистрируетесь как</strong></p>
-                <div className="control">
-                    <div>
-                        <label className="radio">
-                            <input type="radio" name="question" checked/>
-                            <strong className="ml-1">Покупатель</strong>
-                        </label>
+        <div className={s.field}>
+            <form className="box has-background-light">
+                <h5 className="title is-5">
+                    Регистрация
+                </h5>
+                <section className="block m-6">
+                    <p className="mb-1"><strong>Вы регистрируетесь как</strong></p>
+                    <div className="control is-align-content-flex-start">
+                        <div>
+                            <label className="radio">
+                                <input type="radio" name="question" checked/>
+                                <strong className="ml-1">Покупатель</strong>
+                            </label>
+                        </div>
+                        <div>
+                            <label className="radio">
+                                <input type="radio" name="question"/>
+                                <strong className="ml-1">Поставщик</strong>
+                            </label>
+                        </div>
                     </div>
-                    <div>
-                        <label className="radio">
-                            <input type="radio" name="question"/>
-                            <strong className="ml-1">Поставщик</strong>
-                        </label>
+                </section>
+
+
+                <div className={s.formItemBlock }>
+                    <label className="label has-text-weight-normal has-text-right">Эл.почта</label>
+                    <div className={s.wrapper}>
+                        <input className="input is-small" type="email"/>
                     </div>
                 </div>
-            </section>
+                <p><strong>Название организации</strong></p>
+                <div className={s.formItemBlock }>
+                    <label className="label has-text-weight-normal has-text-right">ИП или Юр.лицо</label>
+                    <div className={s.wrapper}>
+                        <input className="input is-small" type="text"/>
+                    </div>
+                </div>
+                <div className={s.formItemBlock }>
+                    <label className="label has-text-weight-normal has-text-right">ИНН</label>
+                    <div className={s.wrapper}>
+                        <input className="input is-small" type="text"/>
+                    </div>
+                </div>
+                <p><strong>Фактический адрес</strong></p>
+                <div className={s.formItemBlock }>
+                    <label className="label has-text-weight-normal has-text-right">Ваш город</label>
+                    <div className={s.wrapper}>
+                        <input className="input is-small" type="text"/>
+                    </div>
+                </div>
+                <div className={s.addressBlock }>
+                    <label className={`label has-text-weight-normal has-text-right ${s.addressLabel}`}>Улица</label>
+                    <div className={s.addressWrapper}>
+                        <div>
+                            <input className="input is-small" type="text"/>
+                        </div>
 
-            <div className="field">
-                <label>Эл.почта</label>
-                <input className="input is-small ml-5" type="url"/>
-            </div>
+                        <div className={"is-flex"}>
+                            <label className="label has-text-weight-normal has-text-right">
+                                <div style={{width: 80}}>Дом/стр.</div></label>
+                            <input className="input is-small ml-4" type="text"/>
+                        </div>
+                       <div className={s.officeBlock}>
+                           <label className="label has-text-weight-normal has-text-right"> Офис</label>
+                           <input className="input is-small ml-4" type="text"/>
+                       </div>
 
-            <p><strong>Название организации</strong></p>
-            <div className="field">
-                <label>ИП или Юр.лицо</label>
-                <input className="input is-small ml-5" type="text"/>
-            </div>
-            <div className="field">
-                <label>ИНН</label>
-                <input className="input is-small ml-5" type="text"/>
-            </div>
-
-            <p><strong>Фактический адрес</strong></p>
-            <div className="field">
-                <label>Ваш город</label>
-                <input className="input is-small ml-5" type="url"/>
-            </div>
-            <div className="field address">
-                <label>Улица</label>
-                <input className="input is-small ml-5" type="url"/>
-                <label>Дом/стр.</label>
-                <input className="input is-small ml-5" type="url"/>
-                <label>Офис</label>
-                <input className="input is-small ml-5" type="url"/>
-            </div>
-            <div className="field">
-                <label>Телефон</label>
-                <div className='phoneField'>
-                    <a className="button is-static is-small ml-5">
-                        +7
-                    </a>
-                    <input className="input is-small" type="tel"/>
+                    </div>
+                </div>
+                <div className={s.formItemBlock }>
+                    <label className="label has-text-weight-normal has-text-right">Телефон</label>
+                    <div className={s.wrapper}>
+                        <button className="button is-static is-small">
+                            +7
+                        </button>
+                        <input className="input is-small" type="tel"/>
+                    </div>
+                </div>
+                <div className={s.formItemBlock }>
+                    <label className="label has-text-weight-normal has-text-right">Сайт вашей компании</label>
+                    <div className={s.wrapper}>
+                        <input className="input is-small" type="url"/>
+                    </div>
+                </div>
+                <p><strong>Виды деятельности</strong></p>
+                <div className={s.formItemBlock}>
+                    <label className={"label has-text-weight-normal has-text-right"}>Магазин</label>
+                    <div className={s.wrapper}>
+                        <input type="checkbox"/>
+                    </div>
+                </div>
+                <div className={s.formItemBlock}>
+                    <label className={"label has-text-weight-normal has-text-right"}>Региональная сеть магазинов</label>
+                    <div className={s.wrapper}>
+                        <input type="checkbox"/>
+                    </div>
+                </div>
+                <div className={s.formItemBlock}>
+                    <label className={"label has-text-weight-normal has-text-right"}>Федеральная сеть магазинов</label>
+                    <div className={s.wrapper}>
+                        <input type="checkbox"/>
+                    </div>
+                </div>
+                <div className={s.formItemBlock}>
+                    <label className={"label has-text-weight-normal has-text-right"}>Федеральная сеть магазинов</label>
+                    <div className={s.wrapper}>
+                        <input type="checkbox"/>
+                    </div>
+                </div>
+                <div className={s.formItemBlock}>
+                    <label className={"label has-text-weight-normal has-text-right"}>Интернет-магазин</label>
+                    <div className={s.wrapper}>
+                        <input type="checkbox"/>
+                    </div>
+                </div>
+                <div className={s.formItemBlock}>
+                    <label className={"label has-text-weight-normal has-text-right"}>Автосервис</label>
+                    <div className={s.wrapper}>
+                        <input type="checkbox"/>
+                    </div>
+                </div>
+                <div className={s.formItemBlock }>
+                    <label className="label has-text-weight-normal has-text-right">Прочее</label>
+                    <div className={s.wrapper}>
+                        <input className="input is-small" type="text"/>
+                    </div>
+                </div>
+                <div className={s.formItemBlock }>
+                    <label className="label has-text-weight-normal has-text-right"></label>
+                    <div className={s.wrapper}>
+                        <ReCAPTCHA className={"mt-6 mb-4"} sitekey="Your client site key"
+                                   onChange={()=>{}} />
+                    </div>
                 </div>
 
-            </div>
-            <div className="field">
-                <label>Сайт вашей компании</label>
-                <input className="input is-small ml-5" type="url"/>
-            </div>
 
-            <p><strong>Виды деятельности</strong></p>
-            <div className="field">
-                <label className="checkbox mr-5">Магазин</label>
-                <input type="checkbox"/>
-            </div>
-            <div className="field">
-                <label className="checkbox mr-5">Региональная сеть магазинов</label>
-                <input type="checkbox"/>
-            </div>
-            <div className="field">
-                <label className="checkbox mr-5">Федеральная сеть магазинов</label>
-                <input type="checkbox"/>
-            </div>
-            <div className="field">
-                <label className="mr-5">Интернет-магазин</label>
-                <input type="checkbox"/>
-            </div>
-            <div className="field">
-                <label className="mr-5">Автосервис</label>
-                <input type="checkbox"/>
-            </div>
-            <div className="field">
-                <label className="mr-5">Прочее</label>
-                <input className="input is-small ml-5"/>
-            </div>
-            <div className='buttonBox'>
-                <button className="button is-info is-small is-fullwidth">Отправить заявку</button>
-            </div>
-        </form>
+                <div className={s.buttonBox}>
+                    <button className="button is-info is-small is-fullwidth">Отправить
+                        заявку
+                    </button>
+                </div>
+            </form>
+        </div>
 
 
     );
